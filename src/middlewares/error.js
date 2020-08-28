5c7bfff3f5cb3c8b280fecd86a92aa4d3d2abeb3
shjang7 <suhy.jang.shj@gmail.com>
2020-08-28 21:21:11 +0900

    Feat - Update error handler with detail message

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -1,11 +1,32 @@
 const createError = require('http-errors');
 
 const errorHandler = (err, req, res, next) => {
-  console.log(err.stack.red);
+  let error = { ...err, name: err.name, message: err.message };
 
-  res.status(500).json({
+  if (process.env.NODE_ENV === 'development') {
+    console.log(`${error.message} | ${error.name}`.brightRed);
+  }
+
+  // mongoose bad ObjectId
+  if (error.name === 'CastError' || error.name === 'NotFoundError') {
+    error.code = 404;
+    error.message = createError(error.code);
+  } else if (error.name === 'ValidationError') {
+    const messages = error.message.split(':');
+    error.code = 400;
+    error.message = createError(messages[messages.length - 1]);
+  } else if (error.code === 11000) {
+    // mongoose duplicate key
+    error.code = 409;
+    error.message = createError('Duplicated field value entered');
+  } else {
+    error.code = 500;
+    error.message = createError(error.code);
+  }
+
+  res.status(error.code).json({
     success: false,
-    error: createError.InternalServerError().message,
+    error: error.message,
   });
 };
 
--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const createError = require('http-errors');
 const quizzes = require('./routes/quizzes');
 const problems = require('./routes/problems');
 const errorHandler = require('./middlewares/error');
@@ -8,6 +9,7 @@ app.use(express.json());
 
 app.use('/api/v1/quizzes', quizzes);
 app.use('/api/v1/problems', problems);
+app.all('*', (req, res, next) => next(createError(400)));
 
 app.use(errorHandler);
 
