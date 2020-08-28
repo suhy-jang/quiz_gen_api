ad5fac4a44e8486e8e912dde5ed9b98fbb5f6aa3
shjang7 <suhy.jang.shj@gmail.com>
2020-08-28 12:42:46 +0900

    Feat - Connect MongoDB with Mongoose

--- src/index.js
+++ src/index.js
@@ -2,9 +2,18 @@ if (process.env.NODE_ENV === 'development') {
   require('dotenv').config({ path: './config/config.env' });
 }
 const app = require('./server');
+const connectDB = require('./db');
+
+connectDB();
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+// Handle unhandled promise rejection
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  server.close(() => process.exit(1));
+});
