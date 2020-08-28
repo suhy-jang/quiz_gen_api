f0b1a73ae62dcc531c396a261eade4c78f753368
shjang7 <suhy.jang.shj@gmail.com>
2020-08-28 11:57:32 +0900

    Feat - Install morgan for dev log

--- src/server.js
+++ src/server.js
@@ -4,4 +4,8 @@ const quizzes = require('./routes/quizzes');
 const app = express();
 app.use('/api/v1/quizzes', quizzes);
 
+if (process.env.NODE_ENV === 'development') {
+  app.use(require('morgan')('dev'));
+}
+
 module.exports = app;
