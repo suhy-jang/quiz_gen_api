38f7d9007941c150e6e9424e5307d5f5860832b0
shjang7 <suhy.jang.shj@gmail.com>
2020-08-31 10:31:46 +0900

    Feat - Install cookie parser

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -11,11 +11,9 @@ exports.authenticate = asyncHandler(async (req, res, next) => {
     req.headers.authorization.startsWith('Bearer')
   ) {
     token = req.headers.authorization.split(' ')[1];
+  } else if (req.cookies.token) {
+    token = req.cookies.token;
   }
-  // else if (req.cookies.token) {
-  //   token = req.cookies.token;
-  // }
-  console.log('cookie', req.cookies);
 
   if (!token) return next(createError(401));
 
